f4f5129ab980a581ed663f4647befa251beda478
Fenrier719 <vlad21072001@gmail.com>
2019-10-14 15:20:17 +0300

    Fixedtask

--- project.py
+++ project.py
@@ -1,26 +1,23 @@
 import collections
 
-def first(list):
-    sum = 0
-    for i in list:
-        sum += i
-    return sum
+def first(my_list_with_numbers):
+    return(sum(my_list_with_numbers)) 
 
-def second(list):
-    return[item for item, count in collections.Counter(list).items() if count > 1]
+def second(my_list):
+    return[item for item, count in collections.Counter(my_list).items() if count > 1]
 
-def third(str):   
-    return(str[::-1])
+def third(str_):   
+    return(str_[::-1])
 
-def fourth(list1,list2):
-    result = list(list1+list2)
+def fourth(my_list1,my_list2):
+    result = list(my_list1+my_list2)
     return(list(collections.OrderedDict.fromkeys(result)))
 
-def fifth(str):
-   return(dict(collections.OrderedDict.fromkeys(str.split())))
+def fifth(str_):
+   return(dict(collections.OrderedDict.fromkeys(str_.split())))
 
-def sixth(str):
-    return(dict(collections.Counter(str.split())))
+def sixth(str_):
+    return(dict(collections.Counter(str_.split())))
     
 
 
