248186954b17b3809e5832b779958c05e0aa19cb
Fenrier719 <vlad21072001@gmail.com>
2019-10-21 15:33:25 +0300

    flowershop

---
+++ flowershop1.py
@@ -0,0 +1,95 @@
+from collections import defaultdict
+
+class Flowers:
+    
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if len(name) == 0:
+            raise ValueError
+        self._name = name         
+
+
+class Bouquet:
+
+    def __init__(self, name, price, flowers=None):
+        self.name = name
+        self.price = price
+        self.flowers = flowers
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if len(name) == 0:
+            raise ValueError
+        self._name = name   
+
+    @property
+    def flowers(self):
+        return self._flowers
+
+    @flowers.setter
+    def flowers(self, flowers):
+        self._flowers = flowers 
+
+class Shop:
+    
+    def __init__(self, name, bouquets=None):
+        self.bouquets = bouquets
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def get_name(self, name):
+        if len(name) == 0:
+            raise ValueError
+        self._name = name
+    
+    
+    def _getbouquet(self, key):
+        self.bouquets[key]
+    
+    def _setbouquet(self, key, value):
+        self.bouquets[key] = value
+
+    def _spare_bouquets(self, bouquet, amount):
+        if amount > self.bouquets[bouquet]:
+            raise ValueError
+
+    def sell(self, bouquet, amount=1):
+        self._spare_bouquets(bouquet, amount)
+        self.bouquets[bouquet] -= amount
+
+    def order(self, bouquet, amount=1):
+        self.bouquets[bouquet] += amount
+    
+            
+
+
+
+
+rose = Flowers('Rose')
+orchid = Flowers('Orchid')
+narcissus = Flowers('Narcissus')
+
+birthday = Bouquet('Birthday', price = 12.3, flowers = {rose.name: 12})
+love = Bouquet('Love', price = 17, flowers = {narcissus.name: 14})
+for_mother = Bouquet('For Mother',price = 25, flowers = {orchid.name: 7})
+
+flower_shop = Shop('Mathias', bouquets = {birthday: 3, love: 7,narcissus: 5})
+ 
+flower_shop.sell(birthday, amount = 3)
+flower_shop.order(birthday, amount = 2)
+flower_shop.sell(birthday, amount = 2)
