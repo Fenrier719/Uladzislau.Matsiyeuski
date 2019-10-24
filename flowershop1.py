a072a3ab9cb214dbd2b1be97df0aa6eeff493fc9
Fenrier719 <vlad21072001@gmail.com>
2019-10-24 14:57:38 +0300

    flowershop

--- flowershop1.py
+++ flowershop1.py
@@ -1,9 +1,9 @@
 from collections import defaultdict
 
-class Flowers:
+class Flower:
     
     def __init__(self, name):
-        self.name = name
+        self.name=name
 
     @property
     def name(self):
@@ -13,15 +13,15 @@ class Flowers:
     def name(self, name):
         if len(name) == 0:
             raise ValueError
-        self._name = name         
+        self._name=name         
 
 
 class Bouquet:
 
     def __init__(self, name, price, flowers=None):
-        self.name = name
-        self.price = price
-        self.flowers = flowers
+        self.name=name
+        self.price=price
+        self.flowers=flowers
 
     @property
     def name(self):
@@ -30,8 +30,8 @@ class Bouquet:
     @name.setter
     def name(self, name):
         if len(name) == 0:
-            raise ValueError
-        self._name = name   
+            raise ValueError('Name  cannot be empty')
+        self._name=name   
 
     @property
     def flowers(self):
@@ -39,34 +39,24 @@ class Bouquet:
 
     @flowers.setter
     def flowers(self, flowers):
-        self._flowers = flowers 
+        self._flowers=flowers 
 
 class Shop:
     
     def __init__(self, name, bouquets=None):
-        self.bouquets = bouquets
-        self._name = name
+        self.bouquets=bouquets
+        self._name=name
 
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def get_name(self, name):
-        if len(name) == 0:
-            raise ValueError
-        self._name = name
-    
-    
+    @property 
     def _getbouquet(self, key):
         self.bouquets[key]
     
     def _setbouquet(self, key, value):
-        self.bouquets[key] = value
+        self.bouquets[key]=value
 
     def _spare_bouquets(self, bouquet, amount):
         if amount > self.bouquets[bouquet]:
-            raise ValueError
+            raise ValueError('There is no such type of bouquet')
 
     def sell(self, bouquet, amount=1):
         self._spare_bouquets(bouquet, amount)
@@ -80,16 +70,16 @@ class Shop:
 
 
 
-rose = Flowers('Rose')
-orchid = Flowers('Orchid')
-narcissus = Flowers('Narcissus')
+rose = Flower('Rose')
+orchid = Flower('Orchid')
+narcissus = Flower('Narcissus')
 
-birthday = Bouquet('Birthday', price = 12.3, flowers = {rose.name: 12})
-love = Bouquet('Love', price = 17, flowers = {narcissus.name: 14})
+birthday = Bouquet('Birthday', price=12.3, flowers={rose.name: 12})
+love = Bouquet('Love', price = 17, flowers={narcissus.name: 14})
 for_mother = Bouquet('For Mother',price = 25, flowers = {orchid.name: 7})
 
-flower_shop = Shop('Mathias', bouquets = {birthday: 3, love: 7,narcissus: 5})
+flower_shop = Shop('Mathias', bouquets={birthday: 3, love: 7, narcissus: 5})
  
-flower_shop.sell(birthday, amount = 3)
-flower_shop.order(birthday, amount = 2)
-flower_shop.sell(birthday, amount = 2)
+flower_shop.sell(birthday, amount=3)
+flower_shop.order(birthday, amount=2)
+flower_shop.sell(birthday, amount=2)
